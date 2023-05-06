19c2dacf5529ad99e98bfd121b3f4e1c6a32a515
kaciczak0211 <kaciczak0211@gmail.com>
2023-05-06 23:42:50 +0100

    which workout day

---
+++ min.js
@@ -0,0 +1,22 @@
+const legs = document.getElementById('legs_day');
+const push = document.getElementById('push_day');
+const pull = document.getElementById('pull_day');
+
+
+function choseLegs(){
+        console.log(legs.value)
+    }
+
+legs.addEventListener('click', choseLegs, true);
+
+function chosePull(){
+    console.log(pull.value)
+}
+
+pull.addEventListener('click', chosePull, true);
+
+function chosePush(){
+    console.log(push.value)
+}
+
+push.addEventListener('click', chosePush, true);
