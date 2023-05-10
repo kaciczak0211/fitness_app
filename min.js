7da504dd34104dc18a2dd140fad64a0c16771c6f
kaciczak0211 <kaciczak0211@gmail.com>
2023-05-10 21:58:46 +0100

    timer

--- min.js
+++ min.js
@@ -1,8 +1,5 @@
-const legs = document.getElementById('legs_day');
-const text = document.getElementById('playerText')
-const push = document.getElementById('push_day');
-const pull = document.getElementById('pull_day');
 
+const text = document.getElementById('playerText')
 const btn = document.querySelectorAll(".btn")
 
 let day;
@@ -12,3 +9,42 @@ btn.forEach(button => button.addEventListener("click", () => {
     text.textContent = `Let's make: ${day}`
 
 }))
+
+// create timer
+let startBtn = document.getElementById("start");
+let pauseBtn = document.getElementById("pause");
+let resetBtn = document.getElementById("reset");
+
+let bgn = 0;
+let interval;
+let timerDisplay = document.getElementById("time");
+
+function start(){
+    let startTime = Date.now() - bgn;
+    interval = setInterval(() => {
+        bgn = Date.now() - startTime;
+        timerDisplay(bgn);
+    }, 10);
+    displayButton("stop");
+}
+
+
+// create list
+
+let addExe = document.getElementById("exe_btn");
+let exeCon = document.getElementById("con");
+let input = document.getElementById("exe");
+
+addExe.addEventListener('click', function(){
+    let newExe = document.createElement('p');
+    newExe.innerText = input.value;
+    exeCon.appendChild(newExe);
+    input.value = "";
+    newExe.addEventListener('click', function(){
+        exeCon.style.textDecoration = "line-through";
+    })
+    newExe.addEventListener('dblclick', function(){
+        exeCon.removeChild(newExe);
+    })
+})
+
